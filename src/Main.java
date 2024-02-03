b6a9325a5ae6fb0ffc6f665eb9362407ad82cbc2
Dmitry Shitik <ximikos93@gmail.com>
2024-02-04 00:37:16 +0300

    987654321

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,30 @@ import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "test.txt";
-        String fullName = "";
-        String dirName = System.getProperty("user.dir");
-        fullName = dirName + File.separator + fileName;
-        System.out.println("File path : " + fullName);
-        File file = new File(fullName);
-
-        if (!file.exists()) {
-            try {
-                if (file.createNewFile())
-                    System.out.println("Don");
-                else
-                    System.out.println("Non");
-            } catch (IOException ex) {
-                Logger.getLogger(Main.class.getName())
-                        .log(Level.SEVERE, null, ex);
-            }
-        } else
-            System.out.println("File already");
-        File dir = new File((dirName + "/tmp/user/java/bin"));
-        dir.mkdirs();
-
-        byte[] b = null;
-        int lenB = 0;
+        InputStream in = null;
+        OutputStream out = null;
+        byte[] buffer = null;
         try {
-            InputStream input = new FileInputStream(file);
-            b = new byte[input.available()];
-            lenB = input.read(b);
+            in = new FileInputStream(new File("D:\\lessons\\testWork\\tmp\\user\\java\\bin\\test.txt"));
+            buffer = new byte[in.available()];
+            in.read(buffer);
+            File file = new File("outputFile.tmp");
+            out = new FileOutputStream(file);
+            out.write(buffer);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName())
                     .log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName())
                     .log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                in.close();
+                out.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
         }
-        System.out.println(lenB==0?"qwe":(char) b[0]);
     }
 }
